f3b3ad375a77fd132302828b762e4d75904a615c
Sentient AI <duplobuster@sentient.ai>
2023-06-12 11:11:54 +0200

    last commit, goodbye

--- DuploBusterDocker/run.sh
+++ DuploBusterDocker/run.sh
@@ -1,1 +1,1 @@
-docker container run -it --network host -v ~/dev/ros_ws:/ros_ws duplobuster-sentient:latest
+docker container run -it --network host -v /dev:/dev --privileged -v ~/dev/ros_ws:/ros_ws duplobuster-sentient:latest
